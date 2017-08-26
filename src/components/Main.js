3ee1011c2c982411931c7c30480e12ea6451885a
macbook pro yifan <yifan@magicmoma.com>
2017-08-26 23:27:08 +0800

    finish 3-6 实现 player 页面，添加 MusicList，控制音量，播放、暂停

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,8 +4,10 @@ import 'normalize.css'
 import '../styles/common.css'
 import '../styles/Main.scss'
 
-import Header from './Header'
-import Progress from './Progress'
+
+import Player from "components/page/Player";
+import Header from "components/Header";
+import {MUSIC_LIST} from "components/config/MusicList";
 
 
 export default class AppComponent extends Component {
@@ -13,52 +15,32 @@ export default class AppComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            // 播放进度 百分比数值
-            progress: '0'
-        };
-        // 总时间
-        this.duration = 0;
+            currentMusicItem: MUSIC_LIST[0]
+        }
     }
 
-
     componentDidMount() {
+        const self = this;
         $('#player').jPlayer({
             ready: function () {
                 $(this).jPlayer('setMedia', {
-                    mp3: "http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3"
+                    mp3: self.state.currentMusicItem.file
                 }).jPlayer('play');
             },
-            supplied: "mp3",
-            wmode: "window",
+            supplied: 'mp3',
+            wmode: 'window',
             useStateClassSkin: true
         });
-        $('#player').bind($.jPlayer.event.timeupdate, (e) => {
-            // 总时间
-            this.duration = e.jPlayer.status.duration;
-            this.setState({
-                progress: e.jPlayer.status.currentPercentAbsolute
-            })
-        });
-    }
-
-    componentWillUnmout() {
-        $('#player').unbind($.jPlayer.event.timeupdate);
-    }
-
-    progressChangeHandler(progress) {
-        console.log('progress:' + progress);
-        $('#player').jPlayer('play', this.duration * progress);
     }
 
 
     render() {
+
+
         return (
             <div className="index">
                 <Header/>
-                <Progress progress={this.state.progress}
-                          progressChangeHandler={this.progressChangeHandler.bind(this)}
-
-                />
+                <Player currentMusicItem={this.state.currentMusicItem}/>
                 <div id='player'></div>
             </div>
         );
---
+++ src/components/page/Player.js
@@ -0,0 +1,110 @@
+import React, {Component} from 'react';
+
+import Progress from '../Progress'
+import './Player.scss'
+
+export default class Player extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            // 播放进度 百分比数值
+            progress: 0,
+            // 音量的值
+            volume: 0,
+            isPlay: true
+
+        };
+        // 总时间
+        this.duration = 0;
+    }
+
+    componentDidMount() {
+        $('#player').bind($.jPlayer.event.timeupdate, (e) => {
+            // 总时间
+            this.duration = e.jPlayer.status.duration;
+            this.setState({
+                progress: e.jPlayer.status.currentPercentAbsolute,
+                // 设置音量
+                volume: e.jPlayer.options.volume * 100
+            })
+        });
+    }
+
+    componentWillUnmout() {
+        $('#player').unbind($.jPlayer.event.timeupdate);
+    }
+
+    // 修改时间的回调
+    changeProgressHandler(progress) {
+        $('#player').jPlayer('play', this.duration * progress);
+    }
+
+    // 修改音量的回调
+    changeVolumeHandler(progress) {
+        $("#player").jPlayer("volume", progress);
+    }
+
+    play() {
+        if (this.state.isPlay) {
+            $("#player").jPlayer("pause");
+        } else {
+            $("#player").jPlayer("play");
+        }
+        this.setState({
+            isPlay: !this.state.isPlay
+        });
+    }
+
+    render() {
+
+        const {id, title, artist, file, cover} = this.props.currentMusicItem;
+        return (
+            <div className="player-page">
+                <h1 className="caption">我的私人音乐坊 &gt;</h1>
+                <div className="mt20 row">
+                    <div className="controll-wrapper">
+                        <h2 className="music-title">{title}</h2>
+                        <h3 className="music-artist mt10">{artist}</h3>
+                        <div className="row mt20">
+                            <div className="left-time -col-auto">-{this.state.leftTime}</div>
+                            <div className="volume-container">
+                                <i className="icon-volume rt" style={{top: 5, left: -5}}></i>
+                                <div className="volume-wrapper">
+
+                                    <Progress
+                                        progress={this.state.volume}
+                                        progressChangeHandler={this.changeVolumeHandler.bind(this)}
+                                        barColor="green"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div style={{height: 10, lineHeight: '10px'}}>
+                            <Progress
+                                progress={this.state.progress}
+                                progressChangeHandler={this.changeProgressHandler.bind(this)}
+                            />
+                        </div>
+                        <div className="mt35 row">
+                            <div>
+                                <i className="icon prev"></i>
+                                <i className={`icon ml20 ${this.state.isPlay ? 'pause' : 'play'}`}
+                                   onClick={this.play.bind(this)}></i>
+
+                                <i className="icon next ml20"></i>
+                            </div>
+                            <div className="-col-auto">
+                                <i className={`icon repeat-${this.props.repeatType}`}></i>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="-col-auto cover">
+                        <img src={cover} alt={title}/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+}
