bcbd1a294c5e03589e6532179e614678a87cddb2
macbook pro yifan <yifan@magicmoma.com>
2017-08-26 19:34:12 +0800

    finish 3-5，eslint add jquery，完善进度条

--- src/components/Main.js
+++ src/components/Main.js
@@ -13,15 +13,18 @@ export default class AppComponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            // 播放进度 百分比数值
             progress: '0'
-        }
+        };
+        // 总时间
+        this.duration = 0;
     }
 
 
     componentDidMount() {
-        $("#player").jPlayer({
+        $('#player').jPlayer({
             ready: function () {
-                $(this).jPlayer("setMedia", {
+                $(this).jPlayer('setMedia', {
                     mp3: "http://oj4t8z2d5.bkt.clouddn.com/%E9%AD%94%E9%AC%BC%E4%B8%AD%E7%9A%84%E5%A4%A9%E4%BD%BF.mp3"
                 }).jPlayer('play');
             },
@@ -29,15 +32,22 @@ export default class AppComponent extends Component {
             wmode: "window",
             useStateClassSkin: true
         });
-        $("#player").bind($.jPlayer.event.timeupdate, (e) => {
+        $('#player').bind($.jPlayer.event.timeupdate, (e) => {
+            // 总时间
+            this.duration = e.jPlayer.status.duration;
             this.setState({
-                progress:e.jPlayer.status.currentPercentAbsolute
+                progress: e.jPlayer.status.currentPercentAbsolute
             })
         });
     }
 
     componentWillUnmout() {
-        $("#player").unbind($.jPlayer.event.timeupdate);
+        $('#player').unbind($.jPlayer.event.timeupdate);
+    }
+
+    progressChangeHandler(progress) {
+        console.log('progress:' + progress);
+        $('#player').jPlayer('play', this.duration * progress);
     }
 
 
@@ -45,7 +55,10 @@ export default class AppComponent extends Component {
         return (
             <div className="index">
                 <Header/>
-                <Progress progress={this.state.progress}/>
+                <Progress progress={this.state.progress}
+                          progressChangeHandler={this.progressChangeHandler.bind(this)}
+
+                />
                 <div id='player'></div>
             </div>
         );
--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,16 +1,39 @@
 import React, {Component} from 'react';
 
 export default class Progress extends Component {
+
+    static defaultProps = {
+        barColor: 'red'
+    };
+
     constructor(props) {
         super(props)
     }
 
+    changeProgress(e) {
+        let {progressBar} = this.refs;
+        let progress = (e.clientX - progressBar.getBoundingClientRect().left) / progressBar.clientWidth;
+
+        // 先判断方法是否存在
+        this.props.progressChangeHandler && this.props.progressChangeHandler(progress);
+
+        e.stopPropagation();
+        e.preventDefault();
+
+    }
+
     render() {
         return (
-            <div className="components-progress">
-                <div className="progress" style={{
-                    width: `${this.props.progress}%`
-                }}></div>
+            <div className="components-progress"
+                 ref="progressBar"
+                 onClick={this.changeProgress.bind(this)}>
+                <div className="progress"
+                     style={{
+                         width: `${this.props.progress}%`,
+                         backgroundColor: this.props.barColor
+                     }}
+
+                ></div>
             </div>
         );
     }
